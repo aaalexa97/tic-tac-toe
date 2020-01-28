1d4ea699d290cac05fcb0fcefb018b9bc2a62fb2
aaalexa97 <alexaa23@uw.edu>
2020-01-28 11:26:42 -0800

    commit

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import './index.css';
 
 class Square extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
     render(){
     return(
-        <button className = "square"> 
+        <button 
+        className = "square" 
+        onclick={() => this.setState({value: 'X'})}
+        > 
+            {this.state.value}
         </button>
     );
     }
@@ -11,14 +22,14 @@ class Square extends React.Component{
 
 class Board extends React.Component{
     renderSquare(i){
-        return <Square />;
+        return <Square value={i} />;
     }
     render() {
         const status = 'Next player:';
 
         return(
             <div>
-                <div className="status">{status} </div>
+                <div className="title">Tic Tac Toe</div>
                 <div className="row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -34,6 +45,8 @@ class Board extends React.Component{
                     {this.renderSquare(7)}
                     {this.renderSquare(8)}
                 </div>
+
+                <div className="status">{status} </div>
             </div>
         );
     }
