e29e31a2d527d49573341898a253074bcf5081e3
aaalexa97 <alexaa23@uw.edu>
2020-01-28 12:21:23 -0800

    commit

---
+++ src/Board.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './index.css';
+import Square from './Square.js';
+
+class Board extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+    renderSquare(i){
+        return <Square value={this.state.squares[i]}/>;
+    }
+    render() {
+        const status = 'Next player: ';
+
+        return(
+            <div>
+                <div className="title">Tic Tac Toe</div>
+                <div className="row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+
+                <div className="status">{status} </div>
+            </div>
+        );
+    }
+}
+
+export default Board;
--- src/Game.js
+++ src/Game.js
@@ -1,56 +1,6 @@
 import React from 'react';
 import './index.css';
-
-class Square extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-    render(){
-    return(
-        <button 
-        className = "square" 
-        onclick={() => this.setState({value: 'X'})}
-        > 
-            {this.state.value}
-        </button>
-    );
-    }
-}
-
-class Board extends React.Component{
-    renderSquare(i){
-        return <Square value={i} />;
-    }
-    render() {
-        const status = 'Next player:';
-
-        return(
-            <div>
-                <div className="title">Tic Tac Toe</div>
-                <div className="row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-
-                <div className="status">{status} </div>
-            </div>
-        );
-    }
-}
+import Board from './Board.js'
 
 class Game extends React.Component{
     render(){
