777907dda759a676de4213512d567075ac8027d0
aaalexa97 <alexaa23@uw.edu>
2020-02-18 21:00:58 -0800

    json

--- src/Board.js
+++ src/Board.js
@@ -13,6 +13,7 @@ const ReturnButton =  withStyles({
       height: 48,
       padding: '0 30px',
       marginTop: '10px',
+      marginBottom: '10px',
       marginLeft: '67px',
       boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
     },
